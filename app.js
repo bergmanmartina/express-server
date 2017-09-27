ee222398eb029b0420cab9adff74f73327a880b4
bergmanmartina <martinabergman91@gmail.com>
2017-09-27 15:16:01 +0200

    ny

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
 	console.log ('nu är vi här')
 res.send('Hello cats')
 })
 app.get('/om-mig', (req, res) => {
-	console.log ('nu är vi här')
+	console.log ('nu är det om mig')
 res.send('Om mig')
 })
 app.listen(3000, (req, res) => {
