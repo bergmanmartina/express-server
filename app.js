103beea2f19b84a845dda2c86bb98d8743e3433d
bergmanmartina <martinabergman91@gmail.com>
2017-09-27 12:27:56 +0200

    ny
    ny

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ app.get('/', (req, res) => {
 	console.log ('nu är vi här')
 res.send('Hello cats')
 })
+app.get('/om-mig', (req, res) => {
+	console.log ('nu är vi här')
+res.send('Hello cats')
+})
 app.listen(3000, (req, res) => {
 console.log('open localhost:3000!')
 })
